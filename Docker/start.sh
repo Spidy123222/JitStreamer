81fb706a651a58cc7ccd8aa352f497ba8bacb4d7
Spidy123222 <64176728+Spidy123222@users.noreply.github.com>
2022-04-24 16:31:12 -0700

    silent netmuxd

--- Docker/start.sh
+++ Docker/start.sh
@@ -1,2 +1,2 @@
-
-/Product/JitStreamer/target/release/jit_streamer ; /Product/netmuxd/target/release/netmuxd
+export RUST_LOG=trace
+/Product/netmuxd/target/release/netmuxd >/dev/null 2>&1 ; /Product/JitStreamer/target/release/jit_streamer
